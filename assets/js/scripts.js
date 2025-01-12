aeaa8d1c671d1674f30e3177a8607919d1ef0c70
Ryan <ryangadinga90@gmail.com>
2025-01-12 17:55:20 +0700

    cleaning code, change colour button, & translate note

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,10 +13,8 @@ function calculate() {
     const result = eval(display.value);
 
     if (result === Infinity) {
-      // display.value = result > 0 ? "∞" : "-∞";
       display.value = "∞";
     } else if (result === -Infinity) {
-      // display.value = "undefined (Invalid operation)";
       display.value = "-∞";
     } else if (isNaN(result)) {
       display.value = "undefined";
@@ -28,12 +26,9 @@ function calculate() {
   }
 }
 
-// Fungsi untuk menambahkan angka atau koma
+// Functions to add numbers or commas
 function appendValue(value) {
-  if (
-    display.value === "" ||
-    display.value === "0"
-  ) {
+  if (display.value === "" || display.value === "0") {
     if (value === ".") {
       display.value = "0.";
     } else if (["+", "-", "*", "/"].includes(value)) {
@@ -42,25 +37,24 @@ function appendValue(value) {
       display.value = value;
     }
   } else {
-    // display.value += value;
-    // Cek jika tombol yang ditekan adalah operator dan jika sudah ada operator sebelumnya
+    // Check if the button pressed is an operator and if there is already a previous operator
     if (["+", "-", "*", "/"].includes(value)) {
-      // Jika operator sudah ada, jangan menggantinya, hanya tambahkan operator berikutnya
+      // If the operator already exists, do not replace it, just add the next operator
       if (
         ["+", "-", "*", "/"].includes(display.value[display.value.length - 1])
       ) {
-        display.value = display.value.slice(0, -1) + value; // Gantikan operator sebelumnya dengan yang baru
+        display.value = display.value.slice(0, -1) + value; // Replace the previous operator with a new one
       } else {
-        display.value += value; // Tambahkan operator ke belakang
+        display.value += value; // Add back operator
       }
     } else {
-      // Untuk angka, tambahkan ke display
+      // For numbers, add them to the display
       display.value += value;
     }
   }
 }
 
-// Fungsi khusus untuk tombol "0"
+// Function for button “0”
 function appendZero() {
   if (display.value === "" || display.value === "0") {
     display.value = "0";
@@ -69,7 +63,7 @@ function appendZero() {
   }
 }
 
-// Fungsi untuk tombol "00"
+// Function for button “00”
 function appendDoubleZero() {
   if (display.value === "" || display.value === "0") {
     display.value = "0";
